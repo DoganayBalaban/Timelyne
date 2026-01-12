5f0f0788a09ce62bebb03cec287b2a48e3e480b3
Doğanay Balaban <balabandoganay@gmail.com>
2026-01-13 02:10:33 +0300

    feature(register): Implement user registration endpoint
    Added logic to the register controller to handle user creation, including input validation, password hashing, and storing user data in the database. Returns appropriate responses for success and error cases.

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,4 +1,29 @@
-export const register = () => {};
+import bcrypt from "bcryptjs";
+import { Request, Response } from "express";
+import { prisma } from "../utils/prisma";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password, firstName, lastName } = req.body;
+    if (!email || !password || !firstName || !lastName) {
+      res.status(400).json({ message: "All fields are required" });
+      return;
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await prisma.user.create({
+      data: {
+        email,
+        password_hash: hashedPassword,
+        first_name: firstName,
+        last_name: lastName,
+      },
+    });
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 export const login = () => {};
 export const logout = () => {};
 export const refresh = () => {};
