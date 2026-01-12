4ba862a5e73813ae5cfc707c14320e4e6a022f7a
Doğanay Balaban <balabandoganay@gmail.com>
2026-01-13 02:21:44 +0300

    feature(jwt): dd JWT token generation to user registration
    Introduces JWT access and refresh token generation upon successful user registration. Also adds duplicate user check and improves error logging in the registration controller.

--- server/src/controllers/authControllers.ts
+++ server/src/controllers/authControllers.ts
@@ -1,16 +1,29 @@
 import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "../config/env";
 import { prisma } from "../utils/prisma";
 
 export const register = async (req: Request, res: Response) => {
   try {
+    //TODO: Validate request body
     const { email, password, firstName, lastName } = req.body;
     if (!email || !password || !firstName || !lastName) {
       res.status(400).json({ message: "All fields are required" });
       return;
     }
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+      return;
+    }
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+    //TODO: Send verification email
     const user = await prisma.user.create({
       data: {
         email,
@@ -19,8 +32,20 @@ export const register = async (req: Request, res: Response) => {
         last_name: lastName,
       },
     });
-    res.status(201).json({ message: "User created successfully", user });
-  } catch (error) {
+    const accessToken = jwt.sign({ userId: user.id }, env.JWT_SECRET, {
+      expiresIn: "15m",
+    });
+    const refreshToken = jwt.sign({ userId: user.id }, env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.status(201).json({
+      message: "User created successfully",
+      user,
+      accessToken,
+      refreshToken,
+    });
+  } catch (error: any) {
+    console.error("❌ User registration error:", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
