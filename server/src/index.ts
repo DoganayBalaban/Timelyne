84a7537ebbaabbdcd3903526aa66c38afcdbc3b7
Doğanay Balaban <balabandoganay@gmail.com>
2026-01-13 01:58:57 +0300

    feature(db): Add database connection and improve server setup
    Introduced a new database connection utility using Prisma and integrated it into the server startup process. Enhanced server security and usability by adding CORS, Helmet, rate limiting, static file serving, and an authentication route.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,34 @@
+import cors from "cors";
 import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import path from "path";
+import { connectDatabase } from "./config/db";
 import { env } from "./config/env";
+import authRoute from "./routes/authRoute";
 
 const app = express();
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on port ${env.PORT}`);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later.",
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/auth", authRoute);
+
+async function startServer() {
+  await connectDatabase();
+
+  app.listen(env.PORT, () => {
+    console.log(`🚀 Server is running on port ${env.PORT}`);
+  });
+}
+
+startServer();
